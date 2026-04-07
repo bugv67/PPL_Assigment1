f3da6082196e716f7cbf08a90015434466791514
Hadar Yanai <yanaih@post.bgu.ac.il>
2026-04-07 19:47:30 +0300

    part 3

--- ex1_template/src/part3.ts
+++ ex1_template/src/part3.ts
@@ -1,4 +1,5 @@
-import { Result, makeFailure, makeOk, bind, either } from "./lib/result";
+import { Result, makeFailure, makeOk, bind, either, isOk } from "./lib/result";
+import * as R from "ramda";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
@@ -10,8 +11,13 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
 
 
 //TO DO- logiclogic -- const mySuccess: Result<number> = makeOk(5);
-export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => undefined as any;
-
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => 
+    R.reduce(
+    ( acc: Result<T>, cur: T) => isOk(acc) ? acc : pred(cur) ? makeOk(cur) : acc,
+       makeFailure<T>("No element found."),
+       a
+     );
+     
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     try {
@@ -23,6 +29,9 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
 }
 
 // TO DO-
-export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => undefined as any;
-export const returnSquaredIfFoundEven_v3 = (a: number[]): number => undefined as any;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> =>
+    bind(findResult(x => x % 2 === 0, a), x => makeOk(x * x));
+;
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number => 
+    either(findResult(x => x % 2 === 0, a), x => x * x, () => -1);
 
